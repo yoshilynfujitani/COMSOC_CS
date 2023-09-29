474e6e5b73a9527595db1be26add43365436ffdc
Yoshi.dev <fujitaniyoshilynp@gmail.com>
2023-09-29 19:42:35 +0800

    Added: Ranking table for users

--- src/Pages/UserPage/Scoreboard.jsx
+++ src/Pages/UserPage/Scoreboard.jsx
@@ -1,7 +1,50 @@
 import React from "react";
+import { FaMedal } from "react-icons/fa";
 
 const Scoreboard = ({ rankingData }) => {
-  return <div>Scoreboard</div>;
+  console.log(rankingData);
+  return (
+    <div className="text-center my-10 md:my-16">
+      <h1 className="text-blue-800 font-semibold text-2xl flex items-center justify-center gap-2">
+        <span className="text-yellow-400 ">
+          <FaMedal />
+        </span>
+        Scoreboard
+      </h1>
+      <p>Top 10</p>
+      <table class="table-fixed bg-white opacity-90 w-full rounded-md text-center ">
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Name</th>
+            <th>Year and Section</th>
+            <th>Points</th>
+          </tr>
+        </thead>
+        <tbody className="gap-2">
+          {rankingData.map((student, index) => (
+            <tr
+              key={index}
+              className={`bg-opacity-40 rounded-md ${
+                index === 0
+                  ? "bg-yellow-400"
+                  : index === 1
+                  ? "bg-slate-600"
+                  : index === 2
+                  ? "bg-amber-700"
+                  : ""
+              }`}
+            >
+              <td>{index + 1}</td>
+              <td>{student.Name}</td>
+              <td>{student.Year_Section}</td>
+              <td>{student.points}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Scoreboard;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,21 @@
 import React from "react";
-
+import CCIS_LOGO from "/CCIS_LOGO.webp";
+import { BsFacebook } from "react-icons/bs";
 const Footer = () => {
-  return <div className="bg-blue-800">Footer</div>;
+  return (
+    <div className=" bg-white   flex items-center justify-center shadow-sm relative z-20 py-2">
+      <div className="flex justify-start w-full flex-col px-5">
+        <div className="flex items-center       ">
+          <img src={CCIS_LOGO} alt="" className="w-12 h-12 " />
+          <div className=" text-gray-800">
+            <h1 className="text-sm font-bold">COMSOC</h1>
+            <p className="text-sm">A.Y 2023-2023</p>
+          </div>
+        </div>
+        <BsFacebook className="transition-all text-2xl mx-2 text-slate-700 hover:text-blue-500" />
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
